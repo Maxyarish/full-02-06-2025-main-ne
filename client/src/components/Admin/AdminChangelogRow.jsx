1452806ccafb89c17266608837afe9d232940a07
max yarish <maxyarish2011@gmail.com>
2025-10-15 22:10:47 +0300

    fixs

--- client/src/components/Admin/AdminChangelogRow.jsx
+++ client/src/components/Admin/AdminChangelogRow.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteChangelogThunk } from '../../store/changelogSlice';
+import styles from './Admin.module.scss';
+import { mdiDelete } from "@mdi/js";
+import Icon from '@mdi/react';
 
 const AdminChangelogRow = ({ changelog }) => {
   const dispatch = useDispatch();
@@ -16,9 +19,16 @@ const AdminChangelogRow = ({ changelog }) => {
       <td>{version}</td>
       <td  style={{ whiteSpace: "pre-wrap" }}>{description}</td>
      <td>{new Date(changelog.releaseDate).toLocaleDateString()}</td>
-      <td>
-        <button onClick={() => handleDelete(_id)}>Delete</button>
-      </td>
+       <td>
+             <button
+               onClick={() => {
+                 handleDelete(_id);
+               }}
+               className={styles["admin-actions"]}
+             >
+               <Icon path={mdiDelete} size={0.7} />
+             </button>
+           </td>
     </tr>
   );
 };
