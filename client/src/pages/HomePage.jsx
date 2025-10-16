4819550494fc0ffc3709ec1798fc977efd1eb6be
max yarish <maxyarish2011@gmail.com>
2025-10-17 01:16:56 +0300

    fix

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -45,7 +45,7 @@ const HomePage = () => {
         amount={amount}
         setAmount={setAmount}
       />
-      <h4 style={{ color: "#55358d" }}>V 1.2.0.2</h4>
+      <h4 style={{ color: "#55358d" }}>V 1.2.0.3</h4>
     </section>
   );
 };
